79094b7180160591d7b23f3420d198c2d7647d73
alexeiepif <aled2093746@gmail.com>
2023-10-17 14:05:31 +0300

    ind2 modificate

--- program/ind2.py
+++ program/ind2.py
@@ -3,6 +3,35 @@
 
 # Вывести на экран большее из трёх заданных чисел.
 
+import sys
+
 if __name__ == "__main__":
     a1, a2, a3 = map(float, input("Введите 3 числа через пробел\n").split())
-    print(f"Большее из трех чисел: {a1, a2, a3} = {max(a1, a2, a3)}")
+
+    if a1 == a2 == a3:
+        print("Все числа равны", file=sys.stderr)
+        exit(1)
+
+    if a1 == a2:
+        print("Числа 1 и 2 равны", file=sys.stderr)
+        exit(1)
+
+    if a2 == a3:
+        print("Числа 2 и 3 равны", file=sys.stderr)
+        exit(1)
+
+    if a1 == a3:
+        print("Числа 1 и 3 равны", file=sys.stderr)
+        exit(1)
+
+    if a1 > a2:
+        if a1 > a3:
+            maximum = a1
+        else:
+            maximum = a2
+    elif a2 > a1:
+        if a2 > a3:
+            maximum = a2
+        else:
+            maximum = a3
+    print(f"Большее из трех чисел: {a1, a2, a3} = {maximum}")
