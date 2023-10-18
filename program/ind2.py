ba102823a22826661b278f9dcbac804fdc798719
alexeiepif <aled2093746@gmail.com>
2023-10-18 14:10:23 +0300

    fix

--- program/ind2.py
+++ program/ind2.py
@@ -8,30 +8,15 @@ import sys
 if __name__ == "__main__":
     a1, a2, a3 = map(float, input("Введите 3 числа через пробел\n").split())
 
-    if a1 == a2 == a3:
-        print("Все числа равны", file=sys.stderr)
-        exit(1)
-
-    if a1 == a2:
-        print("Числа 1 и 2 равны", file=sys.stderr)
-        exit(1)
-
-    if a2 == a3:
-        print("Числа 2 и 3 равны", file=sys.stderr)
-        exit(1)
-
-    if a1 == a3:
-        print("Числа 1 и 3 равны", file=sys.stderr)
-        exit(1)
-
     if a1 > a2:
         if a1 > a3:
             maximum = a1
         else:
-            maximum = a2
-    elif a2 > a1:
+            maximum = a3
+    else:
         if a2 > a3:
             maximum = a2
         else:
             maximum = a3
+        
     print(f"Большее из трех чисел: {a1, a2, a3} = {maximum}")
